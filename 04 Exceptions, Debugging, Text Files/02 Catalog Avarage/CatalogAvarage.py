3428c8b9c38bf05723178927f06197c905f813e0
Plamen Popov <plamenp.popov@hotmail.com>
2016-02-06 12:47:47 +0200

    Exceptions, Debugging, Text Files - Moved catalog CSVs to a Common Resources folder for other tasks to use them as well.

--- 04 Exceptions, Debugging, Text Files/02 Catalog Avarage/CatalogAvarage.py
+++ 04 Exceptions, Debugging, Text Files/02 Catalog Avarage/CatalogAvarage.py
@@ -1,7 +1,7 @@
 sum = 0
 count = 0
 
-with open("catalog_full.csv") as catalog_file:
+with open("../Common Resources/catalog_full.csv") as catalog_file:
     for line in catalog_file:
         values = line.split(sep=',')
         sum += float(values[-1])
