52dcd8567504f5570b9e830db525cea4b131ec11
Plamen Popov <plamenp.popov@hotmail.com>
2016-02-07 23:54:21 +0200

    Packages, Formats etc Search complete.

--- 05 Packages, Formats etc/01 Search/Search.py
+++ 05 Packages, Formats etc/01 Search/Search.py
@@ -1,21 +1,27 @@
 import os
 import sys
 
-
 def main():
     file = sys.argv[1]
     location = sys.argv[2]
+    file_found = False
     if os.path.exists(location):
-        check_files(file, location)
+        file_found = check_files(file, location)
+    else:
+        print("Directory nod found.")
+    if not file_found:
+        print("File \"" + file + "\" not found")
 
 
-def check_files(file, loc):
+def check_files(file, loc, file_found=False):
     for root, dirs, files in os.walk(loc):
+        for directory in dirs:
+            file_found = check_files(file, directory, file_found)
         for name in files:
             if name == file:
                 print(os.path.join(root, name))
-        for dir in dirs:
-            check_files(file, dir)
+                file_found = True
+    return file_found
 
 
 if __name__ == "__main__":
