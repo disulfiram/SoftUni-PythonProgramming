a91c383fe817a220a41819dc9ea5b99f736aaffd
Plamen Popov <plamenp.popov@hotmail.com>
2016-02-13 16:30:10 +0200

    ExamPrep mk2 - Task 2 complete. Turned out catching exception when parsing int was not a good idea.

--- 12 ExamPrep mk2/Task 2 - Caesar cypher/Program.py
+++ 12 ExamPrep mk2/Task 2 - Caesar cypher/Program.py
@@ -2,25 +2,27 @@ import sys
 
 
 def main():
-    key = 0
     try:
-        key = int(input())
+        key = int(input()) % 26
+        message = input()
+        if message.isspace() or message == "":
+            raise Exception
+        encrypted = encrypt_message(key, message)
+        print(''.join(encrypted))
     except:
         print("INVALID INPUT")
-    message = input()
-    encripted = encript_message(key, message)
-    print(''.join(encripted))
 
 
-def encript_message(key, message):
+def encrypt_message(key: int, message: str):
     result = list()
-
     for character in message:
-        if ord(character) >= ord('A') and ord(character) <= ord('Z'):
+        if ord('A') <= ord(character) <= ord('Z'):
             new_char = ord(character) + key
             if new_char > ord('Z'):
                 new_char = ord('A') + new_char - ord('Z') - 1
             result.append(chr(new_char))
+        elif character.isalpha():
+            raise Exception
         else:
             result.append(character)
     return result
